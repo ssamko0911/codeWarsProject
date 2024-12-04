bdde6f3a8694172dd86bea138bb0abe918433f9e
ssamko0911 <ssamko091184@gmail.com>
2024-12-04 22:55:10 +0000

    Add task Object-Oriented PHP #5 - Classical Inheritance - l6

---
+++ src/task122/entity/WebDeveloper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\task122\entity;
+
+class WebDeveloper extends ComputerProgrammer
+{
+    public string $occupation = 'Web Developer';
+    public function __construct(string $name, int $age)
+    {
+        parent::__construct($name, $age);
+    }
+
+    public function describe_job(): string
+    {
+        return sprintf("%s And don't forget to check on my website :D", parent::describe_job());
+    }
+
+    public function describe_website(): string
+    {
+        return 'My professional world-class website is made from HTML, CSS, Javascript and PHP!';
+    }
+}
