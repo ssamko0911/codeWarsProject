5f763719c8e1f0770b7496a49052acf5ba72e356
ssamko0911 <ssamko091184@gmail.com>
2024-12-29 17:27:20 +0000

    Add draft task130

---
+++ src/task130/task130.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+//https://www.codewars.com/kata/577e277c9fb2a5511c00001d/train/php
+
+function vowelShift($text, $n): string
+{
+    return "?";
+}
+
+/*
+
+text = "Thi(2)s i(5)s a(8) te(11)st!"
+n = 1
+output = "Thes is i tast!"
+
+text = "This is a test!"
+n = 3
+output = "This as e tist!"
+
+*/
