7740f1e1a2087d804674fd648f595f86f6204eb1
ssamko0911 <ssamko091184@gmail.com>
2025-02-01 22:04:48 +0000

    Remove entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Run composer install if vendor directory is missing
-if [ ! -d "vendor" ]; then
-  composer install --no-interaction --prefer-dist --optimize-autoloader
-fi
-
-exec "$@"
