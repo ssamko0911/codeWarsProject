8a7b57102853d6622d22e5f32c28a5963f08b058
ssamko0911 <ssamko091184@gmail.com>
2025-06-27 11:43:12 +0100

    Update task - Find the unique number (l6)

--- src/task034/task034.php
+++ src/task034/task034.php
@@ -4,32 +4,7 @@ declare(strict_types=1);
 
 //https://www.codewars.com/kata/585d7d5adb20cf33cb000235/train/php
 
-/* Both solutions are valid, performance issue;
-
-function findUnique(array $numbers): int|float
-{
-    $uniqueNumbers = [];
-    $notUniqueNumbers = [];
-
-    foreach ($numbers as $number) {
-        if (!in_array($number, $uniqueNumbers)) {
-            $uniqueNumbers[] = $number;
-        } else {
-            $notUniqueNumbers[] = $number;
-        }
-    }
-
-    $result = array_filter($uniqueNumbers, function ($value) use ($notUniqueNumbers) {
-       return  $value !== $notUniqueNumbers[0];
-    });
-
-    return current($result);
-}
-*/
-
-//TODO: find an alternative and fast solution;
-
-const UNIQUE_COUNT = 1;
+const ARRAY_LENGTH = 2;
 
 /**
  * @param int|float[] $numbers
@@ -37,58 +12,16 @@ const UNIQUE_COUNT = 1;
  */
 function findUnique(array $numbers): int|float
 {
-    $uniqueNumbers = [];
+    $twoFirstElements = array_slice($numbers, 0, ARRAY_LENGTH);
 
-    foreach ($numbers as $number) {
-        checkNumber($number, $uniqueNumbers);
+    if ($twoFirstElements[0] === $twoFirstElements[1]) {
+        $unique = array_filter($numbers, static function (int $number) use ($twoFirstElements): bool
+        {
+            return $number !== $twoFirstElements[0];
+        });
+
+        return array_values($unique)[0];
     }
 
-    $uniqueAsString = array_search(UNIQUE_COUNT, $uniqueNumbers, true);
-
-    return castString($uniqueAsString);
-}
-
-/**
- * @param int|float $number
- * @param array<string, int> $uniqueNumbers
- * @return void
- */
-function checkNumber(int|float $number, array &$uniqueNumbers): void
-{
-    if (!array_key_exists(strval($number), $uniqueNumbers)) {
-        addNumberAsUnique($number, $uniqueNumbers);
-    } else {
-        incrementValue($number, $uniqueNumbers);
-    }
-}
-
-/**
- * @param int|float $number
- * @param array<string, int> $uniqueNumbers
- * @return void
- */
-function addNumberAsUnique(int|float $number, array &$uniqueNumbers): void
-{
-    $key = strval($number);
-    $uniqueNumbers[$key] = 1;
-}
-
-/**
- * @param int|float $number
- * @param array<string, int> $uniqueNumbers
- * @return void
- */
-function incrementValue(int|float $number, array &$uniqueNumbers): void
-{
-    $key = strval($number);
-    $uniqueNumbers[$key]++;
-}
-
-function castString(string|int $str): int|float
-{
-    if (is_string($str)) {
-        return is_numeric($str) ? floatval($str) : intval($str);
-    }
-
-    return $str;
+    return $twoFirstElements[0] === $numbers[ARRAY_LENGTH] ? $twoFirstElements[1] : $twoFirstElements[0];
 }
