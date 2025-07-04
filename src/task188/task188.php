15f6a20515e1fae786472f06c35180eedb9269ac
ssamko0911 <ssamko091184@gmail.com>
2025-07-04 08:45:15 +0100

    Add task - Almost Even (l6)

--- src/task188/task188.php
+++ src/task188/task188.php
@@ -2,28 +2,29 @@
 
 //https://www.codewars.com/kata/529e2e1f16cb0fcccb000a6b/train/php
 
-function split_integer(int $num, int $parts): array {
-    $array = [];
-
-    if ($num > $parts) {
-        $res = (int) ($num / $parts);
-        $array = array_fill(0, $parts, $res);
-        if ($num % $parts !== 0) {
-            for ($i = count($array) - 1; $i >= 0; $i--) {
-                $array[$i] += 1;
-                if (array_sum($array) === $num) {
-                    return $array;
-                }
-            }
-        }
-    } else {
-
-        $remainder = $parts - $num;
-        $res = (int) ($num / $remainder);
-        $array = [...array_fill(0, $remainder, 0), ...array_fill($remainder, $parts, $res)];
+/**
+ * @param int $num
+ * @param int $parts
+ * @return int[]
+ */
+function split_integer(int $num, int $parts): array
+{
+    if ($num === 0) {
+        return array_fill(0, $parts, 0);
     }
 
-    return $array;
-}
+    $baseNumber = (int) ($num / $parts);
+    $remainder = $num % $parts;
 
-//print_r(split_integer(20,6));
+    $evenNumberGroups = array_fill(
+        0,
+        $parts,
+        $baseNumber
+    );
+
+    for ($i = $parts - 1; $i >= $parts - $remainder; $i--) {
+        $evenNumberGroups[$i]++;
+    }
+
+    return $evenNumberGroups;
+}
