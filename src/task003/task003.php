6e73835fcb27a15739e6f9f8203e700c44e5926b
ssamko0911 <ssamko091184@gmail.com>
2025-07-30 22:44:50 +0100

    Refactor task#003

--- src/task003/task003.php
+++ src/task003/task003.php
@@ -1,15 +1,13 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 //https://www.codewars.com/kata/5a55f04be6be383a50000187/train/php
 
-function isSpecial($n): bool
+function isSpecial(int $number): bool
 {
     $specialDigits = [0, 1, 2, 3, 4, 5];
 
-    foreach (str_split((string)$n) as $digit) {
-        if (!in_array($digit, $specialDigits)) {
+    foreach (str_split((string) $number) as $digit) {
+        if (!in_array($digit, $specialDigits, true)) {
             return false;
         }
     }
