01af63467f9baa835fda198390f00715b7b5fa10
ssamko0911 <ssamko091184@gmail.com>
2026-05-06 13:58:24 +0100

    Added task - Linked Lists - Get Nth Node(l7)

---
+++ src/task213/model/Node.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace App\task213\model;
+
+use InvalidArgumentException;
+
+class Node
+{
+    public $data, $next;
+
+    public function __construct($data, $next = NULL)
+    {
+        $this->data = $data;
+        $this->next = $next;
+    }
+
+    public function get_nth(Node|null $head, int $index)
+    {
+        $copiedNode = $head;
+        $currentNodeIndex = 0;
+
+        while ($currentNodeIndex !== $index && null !== $copiedNode) {
+            $copiedNode = $copiedNode->next;
+            $currentNodeIndex++;
+        }
+
+        if (null === $copiedNode) {
+            throw new InvalidArgumentException('Empty head');
+        }
+
+        return $copiedNode->data;
+    }
+}
---
+++ src/task213/task213.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+//https://www.codewars.com/kata/55befc42bfe4d13ab1000007/train/php
+
+require '../../vendor/autoload.php';
+
+use App\task213\model\Node;
+
+$node = new Node(1, new Node(2, new Node(3)));
+echo $node->get_nth($node, 3) . PHP_EOL;
+echo $node->get_nth($node, 1) . PHP_EOL;
+echo $node->get_nth($node, 2) . PHP_EOL;
