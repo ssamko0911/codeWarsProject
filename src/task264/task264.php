826b476ca0100f656268bc0a15b20cbc0e9b56c3
ssamko0911 <ssamko091184@gmail.com>
2026-05-24 15:39:54 +0100

    Added task - Geometric Progression Sequence (l7)

---
+++ src/task264/task264.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+// https://www.codewars.com/kata/55caef80d691f65cb6000040/train/php
+
+function geometric_sequence_elements(int $start, int $const, int $sequenceLength): string
+{
+    $sequenceString = '';
+    $lastValue = $start;
+
+    for ($i = 0; $i < $sequenceLength; $i++) {
+        $sequenceString .= $lastValue;
+        $lastValue *= $const;
+        $sequenceString .= $i === $sequenceLength - 1 ? '' : ', ';
+    }
+
+    return $sequenceString;
+}
+
+echo geometric_sequence_elements(2, 3, 5).PHP_EOL;
+echo geometric_sequence_elements(1, -2, 10).PHP_EOL;
